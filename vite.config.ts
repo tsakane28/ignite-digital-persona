5c0ba6982a2319f092573ff7d7478a090f8dc6fc
tsakane28 <wesleytsakane116@gmail.com>
2025-04-30 12:30:27 +0200

    feat: update commit, please review  commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ignite-digital-persona/',
   server: {
     host: "::",
     port: 8080,
@@ -18,5 +19,10 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: true,
   },
 }));
